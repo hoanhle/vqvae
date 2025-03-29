23626740eabc6230765ac9368d419d4ffb4da151
Hoanh Le <hoanhle100100@gmail.com>
2025-03-29 23:58:42 +0200

    add vqvae model

--- vqvae.py
+++ vqvae.py
@@ -260,3 +260,75 @@ class VectorQuantizer(nn.Module):
             commitment_loss,
             encoding_indices.view(x.shape[0], -1),
         )
+    
+
+class VQVAE(nn.Module):
+    def __init__(
+        self,
+        in_channels,
+        num_hiddens,
+        num_downsampling_layers,
+        num_residual_layers,
+        num_residual_hiddens,
+        embedding_dim,
+        num_embeddings,
+        use_ema,
+        decay,
+        epsilon,
+    ):
+        super().__init__()
+        self.encoder = Encoder(
+            in_channels,
+            num_hiddens,
+            num_downsampling_layers,
+            num_residual_layers,
+            num_residual_hiddens,
+        )
+        self.pre_vq_conv = nn.Conv2d(
+            in_channels=num_hiddens, out_channels=embedding_dim, kernel_size=1
+        )
+        self.vq = VectorQuantizer(
+            embedding_dim, num_embeddings, use_ema, decay, epsilon
+        )
+        self.decoder = Decoder(
+            embedding_dim,
+            num_hiddens,
+            num_downsampling_layers,
+            num_residual_layers,
+            num_residual_hiddens,
+        )
+
+    def quantize(self, x):
+        z = self.pre_vq_conv(self.encoder(x))
+        (z_quantized, dictionary_loss, commitment_loss, encoding_indices) = self.vq(z)
+        return (z_quantized, dictionary_loss, commitment_loss, encoding_indices)
+
+    def forward(self, x):
+        (z_quantized, dictionary_loss, commitment_loss, _) = self.quantize(x)
+        x_recon = self.decoder(z_quantized)
+        return {
+            "dictionary_loss": dictionary_loss,
+            "commitment_loss": commitment_loss,
+            "x_recon": x_recon,
+        }
+
+
+if __name__ == "__main__":
+    # Create a sample model
+    model_args = {
+        "in_channels": 3,
+        "num_hiddens": 128,
+        "num_downsampling_layers": 2,
+        "num_residual_layers": 2,
+        "num_residual_hiddens": 32,
+        "embedding_dim": 64,
+        "num_embeddings": 512,
+        "use_ema": True,
+        "decay": 0.99,
+        "epsilon": 1e-5,
+    }
+    model = VQVAE(**model_args)
+    # Visualize the model graph
+    dummy_input = torch.randn(1, 3, 64, 64)  # Assuming 3 channel 64x64 images
+    export_to_netron(model, dummy_input, "vqvae_model.onnx")
+
